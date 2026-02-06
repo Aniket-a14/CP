906749d0bbec199feba1e4418d7117e9ed1b396a
Aniket Saha <aniketsahaworkspace@gmail.com>
2026-02-06 11:17:22 +0530

    questions

--- Atcoder/lcs.cpp
+++ Atcoder/lcs.cpp
@@ -1,17 +1,62 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-class Solution{
-    public:
-        string lcs(string& s, string& t){
-            vector<int> dp(s.size(),vector<int>(t.size()+1,-1))
+class Solution
+{
+public:
+    string longestCommonSubsequence(string s1, string s2)
+    {
+        int n = s1.size();
+        int m = s2.size();
+
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= m; j++)
+            {
+                if (s1[i - 1] == s2[j - 1])
+                {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                }
+                else
+                {
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
         }
-};  
 
-int main(){
-    string s,t;
-    cin>>s>>t;
+        string ans = "";
+        int i = n, j = m;
 
-    Solution s;
-    cout<<s.lcs(s,t)<<endl;
+        while (i > 0 && j > 0)
+        {
+            if (s1[i - 1] == s2[j - 1])
+            {
+                ans.push_back(s1[i - 1]);
+                i--;
+                j--;
+            }
+            else if (dp[i - 1][j] > dp[i][j - 1])
+            {
+                i--;
+            }
+            else
+            {
+                j--;
+            }
+        }
+
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
+
+int main()
+{
+    string s, t;
+    cin >> s >> t;
+
+    Solution S;
+    cout << S.longestCommonSubsequence(s, t) << endl;
 }
